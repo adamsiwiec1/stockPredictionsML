7bf3551ee6245da373943938407295394b907d7c
Adam Siwiec <acs0029@mail.wvu.edu>
2021-02-14 07:38:52 -0500

    got DecisionTreeRegressor and KNeighborsRegressor finished - will finish implementing them into one tmr

--- predict.py
+++ predict.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta, date
 import yfinance as yf
 import pandas as pd
 import numpy as np
@@ -15,106 +15,222 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
 
-# This method is for the test class
-def enter_stock(ticker):
-    data = yf.download(ticker, datetime(1994, 1, 1), datetime(2020, 1, 1), interval='1d')
 
-data = yf.download("AZN", datetime(1994, 1, 1), datetime(2020, 1, 1), interval='1d')
-
-# *** 2. Prepare the data *** #
-# Grab only 'Open' data - for our test model purposes
-prices = data[data.columns[0:1]]
-prices.reset_index(level=0, inplace=True)
-
+# Properties
 timestamps = []
-tempPrices = [] # Just in case
 
-# Created my timestamps manually - yfinance ones were messed up
-dateArray = pd.to_datetime(prices['Date'])
-for x in range(len(dateArray)):
-    stringTime = (str(prices['Date'][x]))
-    stringTime = stringTime.split(' ')
-    timestamp = time.mktime(datetime.strptime(stringTime[0], "%Y-%m-%d").timetuple())
-    timestamps.append(timestamp)
 
-for price in prices['Open']:
-    tempPrices.append(price)
+# *** Step 1. Pull the data *** #
+def enter_stock(ticker, startDate, endDate):
+    data = yf.download(ticker, startDate, endDate, interval='1d')
+    return data
+# data = yf.download("AZN", datetime(1994, 1, 1), datetime(2020, 1, 1), interval='1d')
 
-# Take peek
-# print(tempPrices)
-# print(timestamps)
 
-# Drop date from our dataframe
-prices = prices.drop(['Date'], axis=1)
+# **Created timestamps myself - yfinance ones were inconsistent ** #
+def create_timestamps(stockData, dateArray):
+    for x in range(len(dateArray)):
+        stringTime = (str(stockData['Date'][x]))
+        stringTime = stringTime.split(' ')
+        timestamp = time.mktime(datetime.strptime(stringTime[0], "%Y-%m-%d").timetuple())
+        timestamps.append(timestamp)
 
-# Peek our data
 
-prices.append(timestamps)
-prices['Time'] = timestamps
-print(prices['Time'][1])
-print(prices)
+# *** 2. Prepare the data *** # (We are grabbing 'Open' price)
+def prep_data(rawData):
+    stockData = rawData[rawData.columns[0:1]]
+    stockData.reset_index(level=0, inplace=True)
+    dateArray = pd.to_datetime(stockData['Date'])
+    create_timestamps(stockData, dateArray)
+    stockData['Time'] = timestamps  # Add timestamps to our data
+    stockData = stockData.drop(['Date'], axis=1)  # We drop date now that we have timestamp
+    return stockData
+
+
+# Peek data if you want
+def peek(prices):
+    print(prices)
+
+
 # *** 2b. Prepare our model *** #
-dataset = prices.values
-X = dataset[:,1].reshape(-1,1)
-Y = ravel(dataset[:,0:1])  # Ravel changes it from vector to 1D array
+def prep_model(prices):
+    dataset = prices.values
+    X = dataset[:,1].reshape(-1,1)
+    Y = ravel(dataset[:,0:1])  # Ravel changes it from vector to 1D array
+    validation_size = 0.15
+    seed = 7
+    X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size, random_state=seed)
+    return X,Y,X_train,X_validation,Y_train,Y_validation
 
 
-validation_size = 0.20
-seed = 7
-plot = plt.plot(X,Y,'r')
-plt.setp(plot, 'color', 'r', 'linewidth', 0.5)
-plt.figure(1)
-plt.show()
-
-X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size, random_state=seed)
-
-num_folds = 10
-seed = 7
-scoring = "r2"
-scoring2 = 'accuracy'
-
-# Check Algorithms
-models = []
-models.append((' LR ', LinearRegression()))
-models.append((' LASSO ', Lasso()))
-models.append((' EN ', ElasticNet()))
-models.append((' KNN ', KNeighborsRegressor()))
-models.append((' CART ', DecisionTreeRegressor()))
-models.append((' SVR ', SVR()))
-
-# Step 3: Evaluate each model - which one performs the best?
-results = []
-names = []
-for name, model in models:
-    kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=True)
-    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-    # print(cv_results)
-    results.append(cv_results)
-    names.append(name)
-    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-    print(msg)
+def plot_raw_hist(X,Y):
+    plot = plt.plot(X,Y,'r')
+    plt.setp(plot, 'color', 'r', 'linewidth', 0.5)
+    plt.figure(1)
+    plt.show()
 
 
-dates = ["2020-12-23", "2020-12-24", "2020-12-25", "2020-12-26", "2020-12-27",]
-# convert to time stamp
-for dt in dates:
-    stringTime = (str(dt))
-    timestamp = time.mktime(datetime.strptime(stringTime, "%Y-%m-%d").timetuple())
-    # to array X
-    np.append(X, int(timestamp))
+def test_models(X_train,Y_train):
+    num_folds = 10
+    seed = 7
+    scoring = "r2"
+
+    # Check Algorithms
+    models = []
+    models.append((' LR ', LinearRegression()))
+    models.append((' LASSO ', Lasso()))
+    models.append((' EN ', ElasticNet()))
+    models.append((' KNN ', KNeighborsRegressor()))
+    models.append((' CART ', DecisionTreeRegressor()))
+    models.append((' SVR ', SVR()))
+
+    # Step 3: Evaluate each model - which one performs the best?
+    results = []
+    names = []
+    for name, model in models:
+        kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=True)
+        cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+        # print(cv_results)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        print(msg)
+
+
+def predict_cart(X,Y,X_train,Y_train,X_validation,Y_validation, daysToPredict):
+    # get prediction dates
+    base = date.today()
+    dates = [base + timedelta(days=x) for x in range(daysToPredict)]
+    stringDateList = []  # Used to display the date of prediction to user
+
+    # convert to time stamp
+    for dt in dates:
+        stringTime = (str(dt))
+        stringDateList.append(stringTime)
+        timestamp = time.mktime(datetime.strptime(stringTime, "%Y-%m-%d").timetuple())
+        # to array X
+        np.append(X, int(timestamp))
+
+    # Define model
+    model = DecisionTreeRegressor()
+    # Fit to model
+    model.fit(X_train, Y_train)
+    # predict
+    predictions = model.predict(X)
+    print(mean_squared_error(Y, predictions))
+
+    print(len(predictions))
+    leng = len(predictions)
+    count = [0,0]
+    for predict in predictions:
+        count[0]+=1
+        if count[0] > leng - daysToPredict:
+            count[1]+=1
+            print(f'Prediction ({stringDateList[count[1]-1]}) = ' + str(predict))
+
+    # %matplotlib inline
+    fig = plt.figure(figsize=(24, 12))
+    plt.plot(X, Y)
+    plt.plot(X, predictions)
+    plt.show()
+
+def predict_knn(X,Y,X_train,Y_train,X_validation,Y_validation, daysToPredict):
+    # get prediction dates
+    base = date.today()
+    dates = [base + timedelta(days=x) for x in range(daysToPredict)]
+    stringDateList = []  # Used to display the date of prediction to user
+
+    # convert to time stamp
+    for dt in dates:
+        stringTime = (str(dt))
+        stringDateList.append(stringTime)
+        timestamp = time.mktime(datetime.strptime(stringTime, "%Y-%m-%d").timetuple())
+        # to array X
+        np.append(X, int(timestamp))
+
+    # Define model
+    model = KNeighborsRegressor()
+    # Fit to model
+    model.fit(X_train, Y_train)
+    # predict
+    predictions = model.predict(X)
+    print(mean_squared_error(Y, predictions))
+
+    print(len(predictions))
+    leng = len(predictions)
+    count = [0,0]
+    for predict in predictions:
+        count[0]+=1
+        if count[0] > leng - daysToPredict:
+            count[1]+=1
+            print(f'Prediction ({stringDateList[count[1]-1]}) = ' + str(predict))
+
+    # %matplotlib inline
+    fig = plt.figure(figsize=(24, 12))
+    plt.plot(X, Y)
+    plt.plot(X, predictions)
+    plt.show()
+
+
+# Combine two models into one and divide each result before printing
+def predict_cart_and_knn(X,Y,X_train,Y_train,X_validation,Y_validation, daysToPredict):
+    # get prediction dates
+    base = date.today()
+    dates = [base + timedelta(days=x) for x in range(daysToPredict)]
+    stringDateList = []  # Used to display the date of prediction to user
+
+    # convert to time stamp
+    for dt in dates:
+        stringTime = (str(dt))
+        stringDateList.append(stringTime)
+        timestamp = time.mktime(datetime.strptime(stringTime, "%Y-%m-%d").timetuple())
+        # to array X
+        np.append(X, int(timestamp))
+
+    # Define model
+    model = KNeighborsRegressor()
+    # Fit to model
+    model.fit(X_train, Y_train)
+    # predict
+    predictions = model.predict(X)
+    print(mean_squared_error(Y, predictions))  # Print mean sq error
+
+    # Define model 2
+    model = KNeighborsRegressor()
+    # Fit to model 2
+    model.fit(X_train, Y_train)
+    # predict 2
+    predictions2 = model.predict(X)
+    print(mean_squared_error(Y, predictions))  # Print mean sq error 2
+
+    predictList = []
+    predictList2 = []
+
+    leng = len(predictions)
+    count = [0, 0]
+    for predict in predictions:
+        count[0] += 1
+        if count[0] > leng - daysToPredict:
+            count[1] += 1
+            predictList.append(predict)
+    for predict in predictions2:
+        count[0] += 1
+        if count[0] > leng - daysToPredict:
+            count[1] += 1
+            predictList2.append(predict)
+    for x in range(len(predictList)):
+        print(f'Prediction ({stringDateList[x]}) = ' + str((predictList[x]+predictList2[x])/2))
 
 
 
-# Define model
-model = DecisionTreeRegressor()
-# Fit to model
-model.fit(X_train, Y_train)
-# predict
-predictions = model.predict(X)
-print(mean_squared_error(Y, predictions))
 
-# %matplotlib inline
-fig= plt.figure(figsize=(24,12))
-plt.plot(X,Y)
-plt.plot(X,predictions)
-plt.show()
+
+
+    # %matplotlib inline
+    fig = plt.figure(figsize=(24, 12))
+    plt.plot(X, Y)
+    plt.plot(X, predictions)
+    plt.show()
+
+
+
